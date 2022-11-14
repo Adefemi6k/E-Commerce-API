8c4daa3466259129a55433c8e11bc75b11b6a0f0
Adefemi6k <110125538+Adefemi6k@users.noreply.github.com>
2022-11-14 01:34:23 +0100

    Add files via upload

---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,21 @@
+module.exports = {
+        host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'zella.altenwerth21@ethereal.email',
+        pass: 'MDNuhpR8Cb8uTHebMn'
+    }
+}
+
+
+// host: 'smtp.ethereal.email',
+// port: 587,
+// auth: {
+//     user: 'frida.heller@ethereal.email',
+//     pass: 'XR5cVhNCYXnsDfKvBY'
+// }
+
+
+// Name	Zella Altenwerth
+// Username	zella.altenwerth21@ethereal.email (also works as a real inbound email address)
+// Password	MDNuhpR8Cb8uTHebMn
